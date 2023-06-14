f76c5afb1e2ee9fc532562376fb3501949519e95
SamuelWango <wangos478@gmail.com>
2023-06-14 16:05:16 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+//let x = 2000
+//et y = 100
+//function divide(x, y){
+//    return divide
+//}
+//console.log(x/y);
+//divide();
+function divide() {
+    return 2000 / 100;
+  }
+  console.log(2000/100);
+  divide();
+  
+  let x = 2
+  const square = (x) => {
+    return x*x
+  }
+  console.log(x*x);
+  square();
+  
+  let a = 3 
+  let b = 4
+  const add = (a, b) => {
+    return a + b;
+  }
+  console.log(a+b);
+  add();
